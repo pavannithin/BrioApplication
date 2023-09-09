a473955f9694461a1d7bb3a08405d428ed4ad952
jay <ispartan2022@gmail.com>
2023-09-09 10:02:38 +0530

    build fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import AppHeader from './Pages/appHeader';
-import Footer from './Footer/footer';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import AboutContainer from './Pages/AboutContainer';
